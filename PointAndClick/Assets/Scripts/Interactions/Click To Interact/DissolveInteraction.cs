27e22a9c0f1240818edfdd977743acf1122884ec
Jin <Jinyaden@hotmail.com>
2021-02-20 13:30:09 +0000

    Dissolve Interaction
    Created a dissolve shader and wrote the interaction script to have it delete after dissolving

--- PointAndClick/Assets/Scripts/Interactions/Click To Interact/DissolveInteraction.cs
+++ PointAndClick/Assets/Scripts/Interactions/Click To Interact/DissolveInteraction.cs
@@ -2,17 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DissolveInteraction : MonoBehaviour
+namespace COMP1288.PointClick.Jin
 {
-    // Start is called before the first frame update
-    void Start()
+    public class DissolveInteraction : MonoBehaviour, IInteractor
     {
-        
-    }
+        [SerializeField] Material dissolveMaterial = null;
+        private MeshRenderer meshRenderer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        [SerializeField] float dissolveSpeed = 1f;
+
+        private void Awake()
+        {
+            //get references to original material and renderer
+            meshRenderer = GetComponent<MeshRenderer>();
+        }
+
+
+        public void Interact()
+        {
+            //add dissolve material onto object
+            meshRenderer.material = dissolveMaterial;
+
+            //dissolve then delete
+            StartCoroutine(Dissolve());
+        }
+
+        IEnumerator Dissolve()
+        {
+            while (meshRenderer.material.GetFloat("_DissolveAmount") < 1.01f)
+            {
+                meshRenderer.material.SetFloat("_DissolveAmount", meshRenderer.material.GetFloat("_DissolveAmount") + 
+                    dissolveSpeed * Time.deltaTime);
+                yield return null;
+            }
+            if (meshRenderer.material.GetFloat("_DissolveAmount") >= 1.01f)
+            {
+                Destroy(gameObject);
+            }
+        }
     }
 }
